61b01dc84bcf58fbca49b2c3b542d4f518b7e09c
Lakshay Sachdeva <87421868+Lakshay-Sachdeva@users.noreply.github.com>
2022-02-08 02:28:41 +0530

    Initial commit

---
+++ src/components/GroceryInput.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const GroceryInput = () => {
+  return (
+    <div>
+      <h1>Grocery Requirement List</h1>
+      <label>Name</label>
+      <input type="text" name="name" placeholder="Enter Grocery Name" />
+    </div>
+  );
+};
